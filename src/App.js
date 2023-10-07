d0dfed35cbaea56971ff081426cb20b87c2f57b6
Ivor Lewis <ivor.lewis@gmail.com>
2023-10-07 12:57:41 -0600

    fix: fixed the theme to work within webpage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/components/router';
-// import { ThemeProvider } from '@mui/material/styles';
-// import { theme } from './theme/theme';
+import { Button, ThemeProvider, Typography } from '@mui/material';
+import { CssBaseline } from '@mui/material';
+import { theme } from './theme/theme';
 
 function App() {
   return (
-    // <ThemeProvider theme={theme}>
-    <div>
-      <RouterProvider router={router} />
-      <h1>hi</h1>
-    </div>
-    // </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div>
+        <h1>Shma-Shmiji(app component)</h1>
+        <RouterProvider router={router} />
+      </div>
+      <Button variant='contained' color='primary'>
+        Primary
+      </Button>
+      <Button variant='contained' color='secondary'>
+        Secondary
+      </Button>
+      <Button variant='contained' color='success'>
+        Success
+      </Button>
+      <Button variant='contained' color='warning'>
+        Warning
+      </Button>
+      <Button variant='contained' color='info'>
+        Info
+      </Button>
+      <Button variant='contained' color='error'>
+        Error
+      </Button>
+      <Button variant='outlined' color='primary'>
+        Outlined Primary theme
+      </Button>
+      <Typography variant='h1'>test for responsive font sizes</Typography>
+    </ThemeProvider>
   );
 }
 
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,15 +1,42 @@
-// import { createTheme } from '@mui/material/styles';
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+import { colors } from '@mui/material';
 
-// export const theme = createTheme({
-//   palette: {
-//     primary: {
-//       main: '#007bff',
-//     },
-//     secondary: {
-//       main: '#ff4081',
-//     },
-//   },
-//   typography: {
-//     fontFamily: 'Arial, sans-serif',
-//   },
-// });
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#edada3',
+    },
+    secondary: {
+      main: '#f9c7be',
+    },
+    success: {
+      main: '#42899b',
+    },
+    info: {
+      main: '#94c4c1',
+    },
+    warning: {
+      main: '#28405c',
+    },
+    error: {
+      main: '#ce6a6c',
+    },
+  },
+  typography: {
+    fontFamily: 'Arial, sans-serif',
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: 10, // this will change the style for all buttons
+        },
+        outlinedPrimary: {
+          borderRadius: 20,
+        },
+      },
+    },
+  },
+});
+
+export default responsiveFontSizes(theme);
